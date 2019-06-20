b5611e311c0d3b447d3bd20ac963390f72a808a2
anonymous-shy <shenhuayu0417@gmail.com>
2019-06-20 09:47:05 +0800

    commit all

--- Spark220/src/main/scala/xyz/shy/spark220/ml/MLlibStatisticsDemo.scala
+++ Spark220/src/main/scala/xyz/shy/spark220/ml/MLlibStatisticsDemo.scala
@@ -1,5 +1,7 @@
 package xyz.shy.spark220.ml
 
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.stat.{MultivariateStatisticalSummary, Statistics}
 import org.apache.spark.sql.SparkSession
 
 
@@ -16,6 +18,18 @@ object MLlibStatisticsDemo {
       .appName(getClass.getSimpleName)
       .master("local[*]")
       .getOrCreate()
-
+    val sc = spark.sparkContext
+    val observations = sc.parallelize(
+      Seq(
+        Vectors.dense(1.0, 10.0, 100.0),
+        Vectors.dense(2.0, 20.0, 200.0),
+        Vectors.dense(3.0, 30.0, 300.0)
+      )
+    )
+    // Compute column summary statistics.
+    val summary: MultivariateStatisticalSummary = Statistics.colStats(observations)
+    println(summary.mean) // a dense vector containing the mean value for each column
+    println(summary.variance) // column-wise variance
+    println(summary.numNonzeros) // number of nonzeros in each column
   }
 }
