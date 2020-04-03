0f21c94cd7b4a97bc121e84ee6f7d43cc139499f
Shenhuayu <shenhuayu0417@gmail.com>
2020-04-03 18:40:29 +0800

    commit all.

--- TestCode/src/main/java/xyz/shy/App.java
+++ TestCode/src/main/java/xyz/shy/App.java
@@ -1,13 +1,13 @@
 package xyz.shy;
 
+import java.time.LocalDateTime;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println(LocalDateTime.now());
     }
 }
---
+++ TestCode/src/main/java/xyz/shy/T1.scala
@@ -0,0 +1,8 @@
+package xyz.shy
+
+object T1 {
+
+  def main(args: Array[String]): Unit = {
+    println(getClass.getSimpleName)
+  }
+}
